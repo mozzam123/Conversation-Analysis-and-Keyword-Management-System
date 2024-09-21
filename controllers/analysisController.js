199f8c63bbbee9e4001aac2c619d56a4899a7e9a
mozzam123 <mozzam607@gmail.com>
2024-09-22 00:33:06 +0530

    added keyword detection function in utils

--- controllers/analysisController.js
+++ controllers/analysisController.js
@@ -1,5 +1,6 @@
 const keywordsModel = require("./../models/keywordModel");
 const userModel = require("./../models/userModel");
+const {detectKeywords} = require("./../utils")
 
 exports.convoAnalysis = async (req, res) => {
   try {
@@ -17,7 +18,14 @@ exports.convoAnalysis = async (req, res) => {
     if (!exisitngUser) {
       return res.status(404).json({ error: "User not found!" });
     }
-    return res.json({msg: 'sfdj'})
+    const keywords = await keywordsModel.find({user: exisitngUser._id})
+    if (!keywords){
+      return res.status(400).json({error: "No keywords found!"})
+    }
+    response_data = detectKeywords(text,keyword_detection,id)
+    
+    
+    return res.json({ results: response_data });
   } catch (error) {
     console.log({ error: error });
     return res.json({ error: error });
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const keywordsModel = require("./models/keywordModel");
+const userModel = require("./models/userModel");
 
 // Utility function to count the word occurences
 exports.countWords = (text) => {
@@ -12,4 +14,52 @@ exports.countWords = (text) => {
     }
   });
   return wordCounts;
+};
+
+// To detect keywords from text
+exports.detectKeywords = async (text, keywordList, id) => {
+  // Create a dictionary to hold results for each keyword category
+  const responseData = { keyword_detection: {} };
+  const allKeywords = []
+  try {
+    // Fetch all keywords for the given customer_id
+    const keywordObjs = await keywordsModel.find({
+      user: id,
+      name: { $in: keywordList },
+    });
+
+    for (let i = 0; i < keywordObjs.length; i++) {
+      const keywordObj = keywordObjs[i]; // Assuming only one object is returned
+
+     // Loop through each keyword string in keywordObj.keywords array
+      keywordObj.keywords.forEach((keywordString) => {
+
+        // Split the keywords string into an array of individual words
+        const keywords = keywordString.split(",").map((word) => word.trim());
+        
+        // Concatenate these keywords to allKeywords array
+        allKeywords.push(...keywords);
+      });
+    }
+
+    // Split the text into words for easier comparison
+    const textWords = new Set(text.split(/\s+/));
+    
+    
+    // Find matching keywords in the text
+    const foundKeywords = allKeywords.filter((word) => textWords.has(word));
+
+    // Add found keywords to the response data
+    if (foundKeywords.length > 0) {
+      responseData.keyword_detection["Present"] = foundKeywords;
+    } else {
+      responseData.keyword_detection["Present"] = null;
+    }
+    console.log(responseData);
+
+    return responseData;
+  } catch (error) {
+    console.error("Error fetching keywords:", error);
+    throw new Error("Failed to detect keywords");
+  }
 };
